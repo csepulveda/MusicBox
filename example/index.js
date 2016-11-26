70dbd8c9049c8c5777af8a6d96e1a3884c0bd5e8
Jéssica Schissato <jessicaschissato@gmail.com>
2016-11-26 16:58:08 -0300

    get youtube videos from artist

--- example/index.js
+++ example/index.js
@@ -6,11 +6,14 @@ const options = {}
 if (config.has('spotify'))
   options.spotify = config.get('spotify')
 
+if (config.has('youTube'))
+  options.youTube = config.get('youTube')
+
 const musicBox = new MusicBox(options)
 musicBox.search('Andromeda')
 .then((data) => {
   if (data.length) {
-    musicBox.getArtistDetail(data[0].id, 'CL').then(console.log)
+    musicBox.getArtistDetail(data[0], 'CL').then(console.log)
   }
 })
 .catch(console.log)
--- src/index.js
+++ src/index.js
@@ -1,33 +1,36 @@
 const Spotify = require('./lib/spotify')
+const Youtube = require('./lib/youtube')
 
 export class MusicBox {
   constructor (options) {
     if (options.spotify) {
       this.spotify = new Spotify(options.spotify)
     }
+    if (options.youTube) {
+      this.youTube = new Youtube(options.youTube)
+    }
   }
 
   search (query) {
     return new Promise((resolve, reject) => {
-      if (!this.spotify) return reject(new Error('Missing Spotify Connection'))
-      this.searchSpotify(query).then(resolve)
+      if (!this.spotify) return reject(new Error('Missing Spotify connection'))
+      this.spotify.search(query).then(resolve)
     })
   }
 
-  getArtistDetail(id, country) {
+  getArtistDetail(artist, country) {
+    if (!this.youTube) throw new Error('Missing YouTube connection')
     return Promise.all([
-      this.spotify.searchTopTracks(id, country),
-      this.spotify.searchRelatedArtists(id)
-    ]).then(([topTracks, relatedArtists]) => {
+      this.spotify.searchTopTracks(artist.id, country),
+      this.spotify.searchRelatedArtists(artist.id),
+      this.youTube.search(artist.name)
+    ]).then(([topTracks, relatedArtists, videos]) => {
       return {
         topTracks,
-        relatedArtists
+        relatedArtists,
+        videos
       }
     })
-  }
-
-  searchSpotify (query) {
-    return this.spotify.search(query)
   }
 }
 
