ae96fd0601d560952e42899a64bdbbe1f4374f01
Jéssica Schissato <jessicaschissato@gmail.com>
2016-11-26 12:31:27 -0300

    config

---
+++ example/index.js
@@ -0,0 +1,10 @@
+const config = require('config')
+const MusicBox = require('../lib')
+
+const options = {}
+
+if (config.has('spotify'))
+  options.spotify = config.get('spotify')
+
+const musicBox = new MusicBox(options)
+musicBox.search('Led Zeppelin')
--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ export default class MusicBox {
   constructor (options) {
     this.options = options
   }
+
+  search () {
+  }
 }
